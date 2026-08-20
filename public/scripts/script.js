aa8282693d4768fd4b746712e2ab01cf1c13a5dd
Angelaaliyah12 <angel.blom@hva.nl>
2026-08-20 16:14:10 +0200

    #23 button states

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -11,3 +11,86 @@ document.querySelectorAll('.verwijder-fav-form').forEach(form => {
         }, 300); //animatie duur 300ms//
     });
 });
+
+/* bron:https://github.com/fdnd-task/user-experience-enhanced-website/blob/main/docs/client-side-scripting-for-ux.md
+*/const forms = document.querySelectorAll(".notitie-form") /*selecteer de notitie form*/
+
+forms.forEach(form => {/*voor elke form op de pagina, voeg een event listener toe voor het submitten van de form bron: https://www.google.com/search?q=meerdere+formulieren+selecteren+javascript&oq=meerdere+formulieren+selecteren+javas&gs_lcrp=EgZjaHJvbWUqBwgBECEYoAEyBggAEEUYOTIHCAEQIRigATIHCAIQIRifBTIHCAMQIRifBTIHCAQQIRifBTIHCAUQIRifBTIHCAYQIRifBTIHCAcQIRifBdIBCTEyNjM1ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8 (optie 2)*/
+
+    form.addEventListener("submit", async function (event) {
+
+        event.preventDefault() /*voorkom dat de pagina ververst wordt bij het submitten van de form*/
+
+        const button = event.submitter //welk submit knop verstuurd het form//
+
+        // DIS IST SAAAAAAAA OPSLANAAA KNOP
+        if (button.classList.contains("opslaan_knop")) {
+
+            button.classList.add("laden") /*voeg een class toe aan de knop zodat we deze kunnen stylen tijdens het submitte*/
+            button.disabled = true /*zet de button uit zodat er niet meerdere keren op geklikt kan worden tijdens het submitten bron: https://coreui.io/blog/how-to-disable-a-button-in-javascript/*/
+            button.textContent = "opslaan..." /*verander de tekst van de button zodat de gebruiker weet dat er iets gebeurt*/
+
+            const formData = new FormData(form)
+
+            const response = await fetch(form.action, {
+                method: form.method,
+                body: new URLSearchParams(formData)
+            })
+
+            if (response.ok) {
+                button.classList.remove("laden")/*verwijder de loading class*/
+                button.classList.add("opgeslagen")/*voeg class saved toe*/
+                button.disabled = false /*zet de button weer aan*/
+                button.textContent = "opgeslagen✔"
+                //bron:https://wp-mix.com/set-timeout-redirect-javascript/ //
+                setTimeout(function () { window.location = "/favorieten?status=opgeslagen"; }, 400)
+
+            } else {
+                button.classList.remove("laden")/*verwijder de loading class*/
+                button.classList.add("error")/*voeg class error toe*/
+                button.disabled = false
+                button.textContent = "Error ✘"
+                // bron:https://www.youtube.com/watch?v=ZJUhKP9PbHs
+                location.reload(true)
+
+
+            }
+        }
+
+        // DIS IST SI VERWIJDER KNOP//
+        if (button.classList.contains("vw_knop")) { // kijkt of de vw knop is aangeklikt door de class // //bron: https://htmlacademy.org/courses/javascript-basics/conditions-creating-elements/classlist-contains-method//
+
+            button.classList.add("laden")
+            button.disabled = true
+            button.textContent = "verwijderen..."
+
+
+
+            const formData = new FormData(form)
+
+            const response = await fetch(button.formAction, {
+                method: form.method,
+                body: new URLSearchParams(formData)
+            })
+
+            if (response.ok) {
+                button.classList.remove("laden")
+                button.classList.add("opgeslagen")
+                button.disabled = false
+                button.textContent = "verwijderd✔"
+                setTimeout(function () {
+                    window.location = "/favorieten?status=opgeslagen";
+                }, 400)
+
+            } else {
+                button.classList.remove("laden")
+                button.classList.add("error")
+                button.disabled = false
+                button.textContent = "Error ✘"
+                // bron:https://www.youtube.com/watch?v=ZJUhKP9PbHs
+                location.reload(true)
+
+            }
+        }
+    })
+})
